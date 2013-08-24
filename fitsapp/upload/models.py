a3b9695a56f29a58b52c80662715f33a864e0217
Diana Jayne Gonzales <di.gonzales@gmail.com>
2013-08-24 12:38:24 +0800

    fix for upload, hope it works in server

--- fitsapp/upload/models.py
+++ fitsapp/upload/models.py
@@ -4,6 +4,6 @@ from allauth import app_settings
 
 class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
-    uploader = models.ForeignKey(allauth.app_settings.USER_MODEL)
+    uploader = models.ForeignKey(app_settings.USER_MODEL)
     description = models.CharField(max_length=300)
     date_uploaded = models.DateField(auto_now_add=True)
