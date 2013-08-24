0b7bd667729c6b003fffa0c935f1dfec3757739d
Diana Jayne Gonzales <di.gonzales@gmail.com>
2013-08-24 21:53:31 +0800

    added search functionality in uploads

--- fitsapp/upload/urls.py
+++ fitsapp/upload/urls.py
@@ -2,6 +2,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('fitsapp.upload.views',
-    url(r'^search/', 'uploadsearch', name='uploadsearch'),
+    url(r'^search/', 'upload_search', name='upload_search'),
     url(r'^$', 'upload', name='upload'),
 )
--- fitsapp/upload/urls.py~
+++ fitsapp/upload/urls.py~
@@ -2,6 +2,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('fitsapp.upload.views',
-    url(r'^search/', 'upload_search', name='uploadsearch'),
+    url(r'^search/', 'uploadsearch', name='uploadsearch'),
     url(r'^$', 'upload', name='upload'),
 )
