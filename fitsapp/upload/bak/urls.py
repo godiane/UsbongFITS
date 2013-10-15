5e9c530cf88eb9861d1177d6a56b518b25c5f0a3
Diana Jayne Gonzales <di.gonzales@gmail.com>
2013-10-15 15:29:40 +0000

    project cleanup

--- fitsapp/upload/bak/urls.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('fitsapp.upload.views',
-    url(r'^search/', 'upload_search', name='upload_search'),
-    url(r'^$', 'upload', name='upload'),
-)
