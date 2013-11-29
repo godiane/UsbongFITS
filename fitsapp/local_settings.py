df6f77e078a35a3ae5e868fbc984195c01b2efa2
Diana Jayne Gonzales <di.gonzales@gmail.com>
2013-11-30 01:44:50 +0800

    layout adjustment

---
+++ fitsapp/local_settings.py
@@ -0,0 +1,19 @@
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.mysql",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "fits",
+        # Not used with sqlite3.
+        "USER": "root",
+        # Not used with sqlite3.
+        "PASSWORD": "password",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "localhost",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "3306",
+    }
+}
