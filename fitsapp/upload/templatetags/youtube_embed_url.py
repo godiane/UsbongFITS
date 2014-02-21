d93fa0fa2d7db97573fb259d2194667b5af2926c
Diana Jayne Gonzales <di.gonzales@gmail.com>
2014-02-22 01:52:26 +0800

    changes for one or more youtube links

--- fitsapp/upload/templatetags/youtube_embed_url.py
+++ fitsapp/upload/templatetags/youtube_embed_url.py
@@ -10,14 +10,18 @@ import re
 @register.filter(name='youtube_embed_url')
 @stringfilter
 def youtube_embed_url(value):
-    match = re.search(r'(http|https)\:\/\/www\.youtube\.com\/watch\?v\=(\w*)(\&(.*))?', value)
-    if match:
-        embed_url = 'http://www.youtube.com/embed/%s' %(match.group(2))
-        res = "<iframe width=\"560\" height=\"315\" src=\"%s\" frameborder=\"0\" allowfullscreen></iframe>" %(embed_url)
-        #return res
-        replaced_string = re.sub(r'(http|https)\:\/\/www\.youtube\.com\/watch\?v\=(\w*)(\&(.*))?', res, value)
-        return replaced_string
-    return value
+    descriptions = value.split('|')
+    replaced_description = ''
+    for description in descriptions:
+        match = re.search(r'(http|https)\:\/\/www\.youtube\.com\/watch\?v\=(\w*)(\&(.*))?', description)
+        if match:
+            embed_url = 'http://www.youtube.com/embed/%s' %(match.group(2))
+            res = "<iframe width=\"560\" height=\"315\" src=\"%s\" frameborder=\"0\" allowfullscreen></iframe>" %(embed_url)
+            replaced_description = replaced_description + res
+            #re.sub(r'(http|https)\:\/\/www\.youtube\.com\/watch\?v\=(\w*)(\&(.*))?', res, value)
+        else:
+            replaced_description = replaced_description + description
+    return replaced_description
 
 youtube_embed_url.is_safe = True
 
