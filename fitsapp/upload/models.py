5163ed5557d4c512826df8b3376f7d09a309cc03
Diana Jayne Gonzales <di.gonzales@gmail.com>
2014-09-13 23:52:11 +0800

    test fix for bug report on upload/locate/?location=MQ912XKN

--- fitsapp/upload/models.py
+++ fitsapp/upload/models.py
@@ -2,10 +2,11 @@
 from django.db import models
 from allauth import app_settings
 from extra import ContentTypeRestrictedFileField
+from django.contrib.auth.models import User
 
 class Document(models.Model):
     docfile = ContentTypeRestrictedFileField(upload_to='documents/%Y/%m/%d', max_upload_size=3145728)
-    uploader = models.ForeignKey(app_settings.USER_MODEL)
+    uploader = models.ForeignKey(User)
     description = models.CharField(max_length=300)
     date_uploaded = models.DateField(auto_now_add=True)
     download_count = models.PositiveIntegerField(max_length=10, default=0)
@@ -14,10 +15,10 @@ class Document(models.Model):
 
 class Vote(models.Model):
     document = models.ForeignKey('Document')
-    voter = models.ForeignKey(app_settings.USER_MODEL)
+    voter = models.ForeignKey(User)
     date = models.DateField(auto_now_add=True)
 
 class Download(models.Model):
     document = models.ForeignKey('Document')
-    downloader = models.ForeignKey(app_settings.USER_MODEL)
+    downloader = models.ForeignKey(User)
     date = models.DateTimeField(auto_now_add=True)
